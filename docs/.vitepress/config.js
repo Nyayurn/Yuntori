bd597469e79d63ceb6f88b8f9c166a769d396869
Nyayurn <ChenDSakura@163.com>
2024-03-01 15:57:56 +0800

    更新 docs/.vitepress/config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,14 @@
 export default {
     title: 'Yutori-Next-Docs',
     description: 'Yutori-Next\'s Documents',
-
-    themeConfig: {}
+    themeConfig: {
+        siteTitle: 'Yutori Next',
+        sidebar: [
+            { text: "介绍", link: "/README.md" },
+            { text: "指南", link: "/Guild.md" },
+            { text: "进阶", link: "/Advanced.md" },
+            { text: "动作", link: "/Action.md" },
+            { text: "事件", link: "/Event.md" }
+        ]
+    }
 }
